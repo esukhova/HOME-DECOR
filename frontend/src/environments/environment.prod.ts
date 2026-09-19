d63040c54877dc0d2bb62aeabf30fe82da9e4b66
esukhova <e_ukhova@inbox.ru>
2026-09-19 16:01:48 +0300

    Frontend: fix environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   api: 'https://h-decor.shop/api/',
-  serverStaticPath: 'http://h-decor.shop/images/products/'
+  serverStaticPath: 'https://h-decor.shop/images/products/'
 };
